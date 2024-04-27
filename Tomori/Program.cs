42ec9df656bc429ce6e5d1770b7eb6c766059e56
oxy <oxy@dontmailme.com>
2024-04-27 16:09:35 +0200

    Initial setup

---
+++ Tomori/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateApplicationBuilder(args);
+host.Configuration.AddEnvironmentVariables(prefix: "Tomori_");
+host.Services.AddHostedService<DiscordService>();
+
+await host.Build().RunAsync();
+
